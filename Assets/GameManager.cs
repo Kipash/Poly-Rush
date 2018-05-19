cb16f53dbc762a68004866ae894631f512b3d893
Chriss Tman <kipash612@gmail.com>
2018-05-19 11:18:03 +0200

    add from private repository

---
+++ Assets/GameManager.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Node
+{
+    public Color Color
+    {
+        get
+        {
+            return renderers[0].material.color;
+        }
+        set
+        {
+            foreach (var x in renderers)
+            {
+                x.material.color = value;
+            }
+        }
+    }
+    public Transform Transform;
+    public bool Deadly { get; set; }
+    [SerializeField] Renderer[] renderers;
+}
+   
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+    [SerializeField] Transform parent;
+
+    [SerializeField] Color captured;
+    [SerializeField] Color deadly;
+    [SerializeField] Color @default;
+    [SerializeField] float offSet;
+    [SerializeField] float lerpSpeed;
+    [SerializeField] float lerpColorSpeed;
+    [SerializeField] float lerpScaleSpeed;
+    [SerializeField] float lerpMinDiff;
+
+    public bool IsReady { get; private set; }
+
+    [SerializeField] Node origin;
+    [SerializeField] Node left;
+    [SerializeField] Node right;
+    [SerializeField] Transform[] hazardTransforms;
+
+    public bool IsDead { get; set; }
+
+    private void Start()
+    {
+        origin.Transform.position = Vector3.zero;
+        StartCoroutine(Spawn());
+    }
+    IEnumerator Spawn()
+    {
+        left.Deadly = false;
+        right.Deadly = false;
+        var i = UnityEngine.Random.Range(0, 3);
+        switch(i)
+        {
+            case 0:
+                left.Color = deadly;
+                right.Color = @default;
+                left.Deadly = true;
+                break;
+            case 1:
+                left.Color = @default;
+                right.Color = deadly;
+                right.Deadly = true;
+                break;
+            case 2:
+                left.Color = @default;
+                right.Color = @default;
+                break;
+        }
+        left.Transform.position = new Vector3(-offSet, 30);
+        right.Transform.position = new Vector3(offSet, 30);
+        while (true)
+        {
+            left.Transform.position = Vector3.Lerp(left.Transform.position, new Vector3(-offSet, offSet), Time.deltaTime * lerpSpeed);
+            right.Transform.position = Vector3.Lerp(right.Transform.position, new Vector3(offSet, offSet), Time.deltaTime * lerpSpeed);
+
+            if(Vector3.Distance(left.Transform.position, new Vector3(-offSet, offSet)) < lerpMinDiff)
+                break;
+
+            yield return null;
+        }
+        IsReady = true;
+    }
+
+    public bool MoveLeft()
+    {
+        if (left.Deadly)
+            return false;
+
+        IsReady = false;
+        var o = origin;
+        origin = left;
+        left = o;
+        StartCoroutine(Move());
+        return true;
+    }
+    public bool MoveRight()
+    {
+        if (right.Deadly)
+            return false;
+
+        IsReady = false;
+        var o = origin;
+        origin = right;
+        right = o;
+        StartCoroutine(Move());
+
+        return true;
+    }
+    IEnumerator Move()
+    {
+        //origin.Color = captured;
+        while (true)
+        {
+            origin.Transform.position = Vector3.Lerp(origin.Transform.position, Vector3.zero, Time.deltaTime * lerpSpeed);
+            right.Transform.position = Vector3.Lerp(right.Transform.position, Vector3.right * 20, Time.deltaTime * lerpSpeed);
+            left.Transform.position = Vector3.Lerp(left.Transform.position, Vector3.left * 20, Time.deltaTime * lerpSpeed);
+
+            origin.Color = Color32.Lerp(origin.Color, captured, lerpColorSpeed * Time.fixedDeltaTime);
+            right.Color = Color32.Lerp(right.Color, @default, lerpColorSpeed * Time.fixedDeltaTime);
+            left.Color = Color32.Lerp(left.Color, @default, lerpColorSpeed * Time.fixedDeltaTime);
+
+            if (Vector3.Distance(origin.Transform.position, Vector3.zero) < lerpMinDiff)
+            {
+                StartCoroutine(Spawn());
+                break;
+            }
+            yield return null;
+        }
+    }
+
+    public void SetHazardGFX(int max, int value)
+    {
+        if (max == 0)
+            max = 1;
+        if (value == 0)
+            return;
+
+        float s = value / (max / 100f) / 100;
+        Vector3 v3 = Vector3.Lerp(hazardTransforms[0].localScale, new Vector3(s, s, s), lerpScaleSpeed * Time.deltaTime);
+        foreach(var x in hazardTransforms)
+        {
+            x.localScale = v3;
+        }
+    }
+
+    public void Reset()
+    {
+        IsDead = false;
+        IsReady = true;
+        Time.timeScale = 1;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] GameManager gameManager;
+    [SerializeField] int playerStep;
+    [SerializeField] int startHazard;
+    [SerializeField] int hazardStep;
+    [SerializeField] int maxHazard;
+    [SerializeField] float hazardT;
+    [SerializeField] Text scoreUI;
+    int hazard;
+
+    int score;
+
+    private Vector3 fp;   //First touch position
+    private Vector3 lp;   //Last touch position
+    private float dragDistance;  //minimum distance for a swipe to be registered
+
+    private void Start()
+    {
+        Screen.SetResolution(720, 1280, true);
+        dragDistance = Screen.height * 10 / 100; //dragDistance is 15% height of the screen
+
+        hazard = startHazard;
+        InvokeRepeating("Hazard", hazardStep, hazardStep);
+    }
+    void Hazard()
+    {
+        hazard -= hazardStep;
+        if (hazard == 0)
+            gameManager.IsDead = true;
+        hazard = Mathf.Clamp(hazard, 0, maxHazard);
+    }
+    private void Update()
+    {
+        gameManager.SetHazardGFX(maxHazard, hazard);
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveRight();
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Reset();
+        }
+ 
+        if (Input.touchCount == 1) // user is touching the screen with a single touch
+        {
+            Touch touch = Input.GetTouch(0); // get the touch
+            if (touch.phase == TouchPhase.Began) //check for the first touch
+            {
+                fp = touch.position;
+                lp = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Moved) // update the last position based on where they moved
+            {
+                lp = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Ended) //check if the finger is removed from the screen
+            {
+                lp = touch.position;  //last touch position. Ommitted if you use list
+
+                //Check if drag distance is greater than 20% of the screen height
+                if (Mathf.Abs(lp.x - fp.x) > dragDistance || Mathf.Abs(lp.y - fp.y) > dragDistance)
+                {//It's a drag
+                 //check if the drag is vertical or horizontal
+                    if (Mathf.Abs(lp.x - fp.x) > Mathf.Abs(lp.y - fp.y))
+                    {   //If the horizontal movement is greater than the vertical movement...
+                        if ((lp.x > fp.x))  //If the movement was to the right)
+                        {   //Right swipe
+                            MoveRight();
+                        }
+                        else
+                        {   //Left swipe
+                            MoveLeft();
+                        }
+                    }
+                    else
+                    {   //the vertical movement is greater than the horizontal movement
+                        if (lp.y > fp.y)  //If the movement was up
+                        {   //Up swipe
+                            //Debug.Log("Up Swipe");
+                        }
+                        else
+                        {   //Down swipe
+                            //Debug.Log("Down Swipe");
+                        }
+                    }
+                }
+                else
+                {   //It's a tap as the drag distance is less than 20% of the screen height
+                    //Debug.Log("Tap");
+                    Reset();
+                }
+            }
+        }
+    }
+
+    void MoveLeft()
+    {
+        if (!gameManager.IsReady || gameManager.IsDead)
+            return;
+        if (gameManager.MoveLeft())
+        {
+            hazard += playerStep;
+            hazard = Mathf.Clamp(hazard, 0, maxHazard);
+            if ((score / 7f) - (int)(score / 7f) == 0)
+                Time.timeScale += 0.25f;
+            score++;
+            scoreUI.text = score.ToString();
+        }
+    }
+    void MoveRight()
+    {
+        if (!gameManager.IsReady || gameManager.IsDead)
+            return;
+        if (gameManager.MoveRight())
+        {
+            hazard += playerStep;
+            hazard = Mathf.Clamp(hazard, 0, maxHazard);
+            if ((score / 7f) - (int)(score / 7f) == 0)
+                Time.timeScale += 0.25f;
+            score++;
+            scoreUI.text = score.ToString();
+        }
+    }
+
+    void Reset()
+    {
+        if (gameManager.IsDead)
+        {
+            score = 0;
+            hazard = startHazard;
+            gameManager.Reset();
+        }
+    }
+
+}
